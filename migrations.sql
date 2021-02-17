80cb666cd6542be66545be3a56d3df7c649c0d12
Zach Barber <zachbarber321@gmail.com>
2021-02-16 22:27:55 -0500

    alter migrations.sql

--- migrations.sql
+++ migrations.sql
@@ -1,3 +1,12 @@
 CREATE DATABASE employeeDirectory
 USE employeeDirectory
-CREATE TABLE blogs (id INT NOT NULL AUTO_INCREMENT, name CHAR(50) NOT NULL, role CHAR(50) NOT NULL, company CHAR(50) NOT NULL, created_at DATE NOT NULL, deleted_at DATE, PRIMARY KEY (id))
+CREATE TABLE employees (id INT NOT NULL AUTO_INCREMENT, name CHAR(50) NOT NULL, role CHAR(50) NOT NULL, company CHAR(50) NOT NULL, created_at datetime NOT NULL, deleted_at DATE, PRIMARY KEY (id))
+
+INSERT INTO employees (name, role, company, created_at)
+VALUES ('testName1', 'testRole1', 'testCompany1', NOW())
+
+INSERT INTO employees (name, role, company, created_at)
+VALUES ('testName2', 'testRole2', 'testCompany2', NOW())
+
+INSERT INTO employees (name, role, company, created_at)
+VALUES ('testName3', 'testRole3', 'testCompany3', NOW())
