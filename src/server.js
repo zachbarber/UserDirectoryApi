e2ccf7c2f89357b32e72073bb3399e33b0a5bb69
Zach Barber <zachbarber321@gmail.com>
2021-02-16 23:13:52 -0500

    sync

--- src/server.js
+++ src/server.js
@@ -4,17 +4,29 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import mysql from 'mysql';
 
+
 global.__dirname = path.resolve('./');
+
 
 const app = express();
 
+
 const PORT = process.env.PORT || 35565;
 
+
 const connection = mysql.createConnection({
-  host: `localhost/${PORT}`,
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'employees'
 });
 
+
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, '../../employee-directory-web/build')));
+
+app.use(bodyParser.json());
 
 
 app.get('/api/companies', (req, res) => {
@@ -36,5 +48,5 @@ app.put('/api/users', (req, res) => {
 
 
 app.listen(PORT, () => {
-  console.log(`listening on ${PORT}`);
+  console.log(`listening on ${PORT}..`);
 });
